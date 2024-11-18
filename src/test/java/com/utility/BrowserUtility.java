b06816d13cbed2655393c2305aa36b2fe733dd41
Ritesh Ranjan Mishra <riteshranjanmishra938@gmail.com>
2024-11-19 01:42:31 +0530

    update the path of file to use relative path

--- src/test/java/com/utility/BrowserUtility.java
+++ src/test/java/com/utility/BrowserUtility.java
@@ -130,7 +130,7 @@ public class BrowserUtility {
 
         String timeStamp = simpleDateFormat.format(date);
 
-        String path ="./screenshots/"+fileName+"-"+timeStamp+".png";
+        String path ="screenshots/"+fileName+"-"+timeStamp+".png";
 
         File file = new File(path);
 
